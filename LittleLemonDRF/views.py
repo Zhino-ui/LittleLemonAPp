28f56dae7e47b596832f5414f431938be837addd
Maxwell Asante <zhino4u2010@gmail.com>
2023-07-10 17:00:36 +0000

    authentication done

--- LittleLemonDRF/views.py
+++ LittleLemonDRF/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
 from .models import MenuItem, Category
+from rest_framework.response import Response
+from rest_framework.decorators import api_view,renderer_classes
 from .serializers import MenuItemSerializer, CategorySerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
 
 class CategoriesView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
@@ -12,3 +16,19 @@ class MenuItemsView(generics.ListCreateAPIView):
     ordering_fields = ['price', 'inventory']
     filterset_fields = ['price', 'inventory']
     search_fields = ['title']
+
+
+# creating a protected api endpoint
+@api_view()
+@permission_classes([IsAuthenticated])
+def secret(request):
+    return Response({"message":"some secret message"})
+
+# creating manager view
+@api_view()
+@permission_classes([IsAuthenticated])
+def manager_view(request):
+    if request.user.groups.filter(name='Manager').exists():
+        return Response({"message":"Only manager should see this"})
+    else:
+        return Response({"message":"You are not authorised"}, 403)
